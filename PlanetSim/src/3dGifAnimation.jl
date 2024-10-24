ccd02c760b40dd66854c517873cafa87e15bdad6
Elijah Phifer <ephifer21@gmail.com>
2024-10-24 12:11:12 -0500

    new docs

--- PlanetSim/src/3dGifAnimation.jl
+++ PlanetSim/src/3dGifAnimation.jl
@@ -10,10 +10,10 @@ export run
 simu = set_Simulation(30_000.0, 2.95912208286e-4)  # t_end, G
 
 # Add planets
-add_planet!(simu, Planet("Sun", 1.00000597682, [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]))
+add_planet!(simu, Planet("Sun", 2.00000597682, [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]))#1.00000597682
 add_planet!(simu, Planet("Jupiter", 0.000954786104043, [-3.5023653, -3.8169847, -1.5507963], [0.00565429, -0.0041249, -0.00190589]))
 add_planet!(simu, Planet("Saturn", 0.000285583733151, [9.0755314, -3.0458353, -1.6483708], [0.00168318, 0.00483525, 0.00192462]))
-add_planet!(simu, Planet("Uranus", 0.0000437273164546, [8.310142, -16.2901086, -7.2521278], [0.00354178, 0.00137102, 0.00055029]))
+add_planet!(simu, Planet("Uranus", 0.0000437273164546, [8.310142, -16.2901086, -7.2521278], [0.00354178, 0.00137102, 0.00055029])) #0.0000437273164546
 add_planet!(simu, Planet("Neptune", 0.0000517759138449, [11.4707666, -25.7294829, -10.8169456], [0.0028893, 0.00114527, 0.00039677]))
 add_planet!(simu, Planet("Pluto", 1 / 1.3e8, [-15.5387357, -25.2225594, -3.1902382], [0.00276725, -0.00170702, -0.00136504]))
 
@@ -47,7 +47,7 @@ function run()
         frame(anim)
     end
 
-    gif(anim, "animations/planetary_orbits_1.gif", fps=15)
+    gif(anim, "animations/planetary_orbits_2.gif", fps=15)
     display(anim)
 end
 
